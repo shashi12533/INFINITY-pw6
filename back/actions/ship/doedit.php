73116a412c2ffdf6f11f1cbe84490b9cba2a1602
WebFaker <nicolas.seyha@gmail.com>
2018-06-20 16:32:39 +0200

    ✨ Added image feature and some stuff

--- back/actions/ship/doedit.php
+++ back/actions/ship/doedit.php
@@ -2,13 +2,13 @@
 // Require the file connexion.php to connect to the database
 require_once "../../includes/connection.php";
 // Updating the values below from the "planetes" table where there's the needed id
-$sql = "UPDATE `db_space`.`planets` SET `name`=:name, `size`=:size, `air`=:air, `wildlife`=:wildlife, `flora`=:flora, `weather`=:weather, `water`=:water, `work`=:work, `link`=:link WHERE `id`=:id
+$sql = "UPDATE `db_space`.`ship` SET `name`=:name, `size`=:size, `air`=:air, `duration`=:duration, `spacing`=:spacing, `connection`=:connection, `installations`=:installations, `link`=:link WHERE `id`=:id
 ;";
 
-if(empty($_POST['name']) OR empty($_POST['size']))
+if(empty($_POST['name']) OR empty($_POST['size']) OR empty($_POST['air']) OR empty($_POST['duration']) OR empty($_POST['spacing']) OR empty($_POST['connection']) OR empty($_POST['installations']))
     {
     $msg = 'error';
-    header("Location: ../../index.php?msg=".$msg);
+    header("Location: ../../index.php?msg=".$msg."#ship");
     }
 
 // All the needed fields are set, we're now entering into the database
@@ -21,11 +21,10 @@ $stmt->bindParam(":id", $_POST['id']);
 $stmt->bindValue(':name', $_POST['name']);
 $stmt->bindValue(':size', $_POST['size']);
 $stmt->bindValue(':air', $_POST['air']);
-$stmt->bindValue(':wildlife', $_POST['wildlife']);
-$stmt->bindValue(':flora', $_POST['flora']);
-$stmt->bindValue(':weather', $_POST['weather']);
-$stmt->bindValue(':water', $_POST['water']);
-$stmt->bindValue(':work', $_POST['work']);
+$stmt->bindValue(':duration', $_POST['duration']);
+$stmt->bindValue(':spacing', $_POST['spacing']);
+$stmt->bindValue(':connection', $_POST['connection']);
+$stmt->bindValue(':installations', $_POST['installations']);
 $stmt->bindValue(':link', $_POST['link']);
 $stmt->execute();
 
@@ -36,6 +35,6 @@ if ($stmt->errorCode() !== '00000') {
 
 $msg = 'edited';
 
-header("Location: ../../index.php?msg=".$msg."#planet");
+header("Location: ../../index.php?msg=".$msg."#ship");
 
     }
