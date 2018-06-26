83a173771ec6498d5955d6eda56d2b266d89dba8
ShAd0wInK <alexandredelaloy@live.com>
2018-06-27 01:52:51 +0200

    🚧 Add custompacks.html structure
    fix issue #66

--- src/js/app.js
+++ src/js/app.js
@@ -3,29 +3,20 @@ import './ui';
 
 console.log('app.js is running ! 💛');
 
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
-
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-});
-
-var packs = document.querySelector('.aio_packs');
-
-
-// window.addEventListener('resize', function() {
-//     if (window.innerWidth >= 600) {
-//         packs.setAttribute('data-flickity','{ "draggable": false }');
-//         console.log('cc')
-    
-//     } else {
-//         packs.setAttribute('data-flickity','{ "wrapAround": true }');
-//         console.log('ets')
-//     }
-// });
+var elem = document.querySelectorAll('.main-carousel');
+for (let i = 0; i < elem.length; i++) {
+  elem[i];
+  var flkty = new Flickity( elem[i], {
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
+  
+  // element argument can be a selector string
+  //   for an individual element
+  var flkty = new Flickity( '.main-carousel', {
+    // options
+  });
+  
+  
+}
